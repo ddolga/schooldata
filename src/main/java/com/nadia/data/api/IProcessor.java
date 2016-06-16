4deeabf16e4bf954b346d52b0820c39770a2daa4
Denis Dolgachev <ddolgachev@gmail.com>
2016-06-16 08:05:53 -0400

    wp changes

---
+++ src/main/java/com/nadia/data/api/IProcessor.java
@@ -0,0 +1,7 @@
+package com.nadia.data.api;
+
+/**
+ * Created by Denis on 6/9/16.
+ */
+public interface IProcessor {
+}
---
+++ src/test/java/com/nadia/data/processors/spider/NsiTest.java
@@ -0,0 +1,16 @@
+package com.nadia.data.processors.spider;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Denis on 6/10/16.
+ */
+public class NsiTest {
+    @Test
+    public void doYaThing() throws Exception {
+
+    }
+
+}
